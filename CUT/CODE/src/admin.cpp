ad98b99fa93bca22a91a5090ec90bc0807de7067
Yashwanthi16 <114207982+Yashwanthi16@users.noreply.github.com>
2022-10-12 13:01:42 +0530

    Update admin.cpp

--- CUT/CODE/src/admin.cpp
+++ CUT/CODE/src/admin.cpp
@@ -3,11 +3,9 @@
 #include <cstring>
 #include <fstream>
 #include <map>
-#include <bits/stdc++.h>
-
+#include <sstream>
 using namespace std;
-
-#include "../header/admin.h"
+#include <admin.h>
 
 #define success 1
 #define failure 0
