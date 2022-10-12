70636f22cf61ea3b3d973965276ef362ec48e235
SamsonpaulM <113003758+SamsonpaulM@users.noreply.github.com>
2022-10-12 10:49:19 +0530

    Update user.h

--- CUT/CODE/header/user.h
+++ CUT/CODE/header/user.h
@@ -10,30 +10,23 @@ using namespace std;
 #include "../header/businessContacts.h"
 
 class User
-{
-	map <int, Personal> pmap;
-	map <int, Business> bmap;
-	map <string, string> umap;
-	
+{	
 	public:
-	void loadUserdatabase1();
-	int checkDate(string );
-	int checkContact(string );
-	void loadContactdatabase(string );
-	void changePassword(string );
-	void addContact();
-	void updateContact();
-	void deleteContact();
-	void listPersonalcontacts();
-	void listBusinesscontacts();
-	void userLogout(string );
+		void loadUserdatabase1();
+		int checkDate(string );
+		int checkContact(string );
+		void loadContactdatabase(string );
+		void changePassword(string );
+		void addContact();
+		void updateContact();
+		void deleteContact();
+		void listPersonalcontacts();
+		void listBusinesscontacts();
+		void userLogout(string );
+	private: 
+		map <int, Personal> pmap;
+		map <int, Business> bmap;
+		map <string, string> umap;
 };
 
 #endif
-
-			
-
-
-
-
-
